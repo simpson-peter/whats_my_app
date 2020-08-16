619f80e80673cd7bbcbf3aed1abd430b5873d79c
simpson-peter <simpson.petere@gmail.com>
2020-08-16 17:53:16 -0400

    Implemented title animation

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const TextStyle kDisplayTextStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 45,
+  fontWeight: FontWeight.w700,
+);
--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whats_my_app/constants.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomePage extends StatelessWidget {
   @override
@@ -7,18 +9,22 @@ class WelcomePage extends StatelessWidget {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
-          end: Alignment.topCenter,
-          stops: [0.05, 0.2, 1],
+          end: Alignment.bottomRight,
+          stops: [0.05, 0.1, 0.6, 1],
           colors: [
             Color(0xFFfcddc0),
             Color(0xFFFFDAB9),
+            Color(0xFFffba7a),
             Color(0xFFffc087),
           ],
         ),
       ),
       child: Center(
-        child: Text(
-          'What\'s My App Bro?',
+        child: TypewriterAnimatedTextKit(
+          speed: Duration(milliseconds: 200),
+          totalRepeatCount: 1,
+          text: ['What\'s My App, Bro?'],
+          textStyle: kDisplayTextStyle,
         ),
       ),
     );
