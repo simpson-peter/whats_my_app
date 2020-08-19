f61ad48b66c54eb58dee3e2408e336859cf47037
simpson-peter <simpson.petere@gmail.com>
2020-08-19 14:10:32 -0400

    beta build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "748e8de8b35b4ba380e62f44726aa6b1",
 "/": "748e8de8b35b4ba380e62f44726aa6b1",
-"main.dart.js": "bfc6ff2fff41cde79509e6ff7e29dae4",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "d64a009cbb07fd1c0897df375eee218d",
+"favicon.png": "b515122c7ae11c4d97abad150710e9d6",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "e71d62502e84c73b81d559b8e25d7a8f",
